2118c7b719eb55ade012c251e83523e99a721906
pbishop3 <pbishop3@gmu.edu>
2025-01-09 13:20:46 -0500

    Cleaned up code, added comments for support functions.

--- RealF.m
+++ RealF.m
@@ -1,4 +1,5 @@
-%Iterative Billiard map for real value case. 
+% Iterative Billiard map for real value case. Takes in current theta value
+% and r value and outputs next r value and theta value.  
 % INPUT: input = [r, theta] where r = cos(gamma) and theta is parameter value 
 %                corresponging to initial point on boundary.
 %        coefx = the coefficients for the cosine terms in the x coordinate 
@@ -19,45 +20,35 @@ flag = 0;
 v1 = r2v2(r1,th1,dz1/norm(dz1));
 v=priorv(v1,dz1);
 
-
-
-[distguess,thguess] = nextstepell(th1,v,coefx(1),coefy(1));
-	
+% Create guess for next newtons method
+[distguess,thguess] = nextstepell(th1,v,coefx(1),coefy(1));	
 guess = [distguess;thguess];    
-
-[~,dz] = Bill_Table(th1,coefx,coefy); 
-
-
-[pts2,v,rtemp] = findbouncevector(th1,v,coefx,coefy);
-
-    
+% Construct function from vector v and boundary. Use Newton's method to
+% find point of intersection.
+[pts2,v,~] = findbouncevector(th1,v,coefx,coefy);
 L =@(s) pts2 + s.*v;
 
 F =@(x) L(x(1)) - Bill_Table(x(2),coefx,coefy);
-
 dF = @(x) [v,-dBill_Table(x(2),coefx,coefy)];
-    
+
+% Apply deflation to avoid converging to point of origin.     
 deflat =  @(x) (1/norm([x(1)-0;x(2)-th1])^2);
 ddeflat = @(x) -2*[x(1),x(2)-th1]*deflat(x)^2;
 	
 G = @(x) F(x)*deflat(x);
 dG =@(x) F(x)*ddeflat(x)+deflat(x)*dF(x);
 
-[newtval,resid,kk] = Newtons(G,dG,guess);
-    
-    if kk > 999
-        flag =1;
-        
-    end
-    
+[newtval,~,kk] = Newtons(G,dG,guess);
+
+% Find new r value and save both [r,th]
 th2 = newtval(2);
 [~,dz2] = Bill_Table(th2,coefx,coefy);	
-
 r2 = v2r(v,dz2); 
 output = [r2;th2];
 
 
-%%%%			
+%% nextstepell
+% Finds the next point of contact on the ellipse. 
 function [tell,th2] = nextstepell(th1,v1,a,b)
 	%Use the previous v so we can figure out the ellipse bounce instead of the actual bounce
 	tpi = 2*pi;
@@ -69,7 +60,8 @@ function [tell,th2] = nextstepell(th1,v1,a,b)
 	th2 = mod(atan2(sin(tpi*th1)+tell*dell(2),cos(tpi*th1)+tell*dell(1)),tpi)/tpi;
 
 	
-%%%%	
+%% findbouncevector
+% Law of reflection
 function [pts,v,r] = findbouncevector(th,v1,coefx,coefy)
     % Find normal vector to p2
     [pts,n1] = Bill_Table(th,coefx,coefy);
